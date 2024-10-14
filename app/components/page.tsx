44c00f037b75d89164283beadef1a70961056ccf
unknown <ambreenjahan144@gmail.com>
2024-10-14 21:13:45 +0500

    change proptype

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,26 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import propTypes from "prop-types";
 
-type propsType={
-    name: string
-    capital: string
-    population:string
-    flag:string
-    website:string
-  
-
+type propTypes={
+    name: any;
+    capital:any;
+    population:any;
+    flag:any;
+    website:any
 }
 
-const Display = (props:propsType)=> {
+const Display = ({name, capital, population, flag, website}:propTypes)=>{
+
+
   return(
     
     <div>
         
-    <h2 className="bg bg-green-50 mt-4 font-bold font-serif text-center ml-10 mr-9"> {props.name}<br/> {props.capital} <br/> {props.population} <br/>  {props.flag && <img src={props.flag} alt={`${props.flag} flag`} />} <br/><Link href={props.website}>
-                    <div className="text-blue-500 underline">{props.website}</div> </Link></h2>
+    <h2 className="bg bg-green-50 mt-4 font-bold font-serif text-center ml-10 mr-9"> {name}<br/>{capital} <br/> {population} <br/>  {flag && <img src={flag} alt={`${flag} flag`} />} <br/><Link href={website}>
+                    <div className="text-blue-500 underline">{website}</div> </Link></h2>
     </div>
   )
 }
-
 
  export default  Display;
